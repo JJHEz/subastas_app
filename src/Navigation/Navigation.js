7b05c719196d1bb5cb91dd2f78323a2c50d8d39a
David <davidhuancaledezma@gmail.com>
2025-04-05 23:57:22 -0400

    Add: visualización de producto en subasta

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -1,13 +1,14 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import Home from "../screens/home";
-
+import ProductoEnSubasta from "../screens/productoEnSubasta";
 const Stack = createNativeStackNavigator();
 
 function MyStack() {
     return (
-        <Stack.Navigator initialRouteName="Home">
+        <Stack.Navigator initialRouteName="ProductoEnSubasta">
             <Stack.Screen name="Home" component={Home} options={{ title: "Pantalla Principal" }}/>
+            <Stack.Screen name="ProductoEnSubasta" component={ProductoEnSubasta} options={{ title: "Subasta en linea" }}/>
         </Stack.Navigator>
     );
 }
---
+++ src/screens/productoEnSubasta.js
@@ -0,0 +1,107 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, Button, TextInput, Image, StyleSheet } from 'react-native';
+import database from "../config/firebase"
+import { collection, addDoc, getDocs, getDoc, doc } from 'firebase/firestore';
+import { getStorage, ref, getDownloadURL } from 'firebase/storage';
+
+
+export default function ProductoEnSubasta() {
+
+/*
+    const fetchUsuarios = async () => {
+        try {
+            const querySnapshot = await getDocs(collection(database, 'producto'));
+            const listaUsuarios = [];
+            querySnapshot.forEach((doc) => {
+                listaUsuarios.push({ id: doc.id, ...doc.data() });
+                console.log(listaUsuarios);
+            });
+            //setUsuarios(listaUsuarios);
+        } catch (error) {
+            console.error("Error al obtener los Productos: ", error);
+        }
+    };
+    */
+
+    // Estado para almacenar los datos del producto
+    const [producto, setProducto] = useState(null);
+
+
+    const getProductoPorID = async () => {
+        try {
+            const productoID = "2"; // <-- aquí pones el ID específico
+            const docRef = doc(database, 'producto', productoID);
+            const docSnap = await getDoc(docRef);
+
+            const datosObtenidos = docSnap.data();
+            setProducto({idProducto:docSnap.id, ...datosObtenidos});
+
+            // codigo para obtener la url de una imagen del firebase
+            const storage = getStorage();
+            const imagenRef = ref(storage,'laptop.jpg');
+            const url = await getDownloadURL(imagenRef);
+            console.log("URL DE LA IMAGEN:" + url);
+                
+            console.log("Producto encontrado:", docSnap.data());
+            console.log("Id del producto: " + docSnap.id);
+            
+        } catch (error) {
+            console.error("Error al obtener el producto: ", error);
+        }
+    };
+
+    // Ejecutar la función automáticamente cuando el componente se monta
+    useEffect(() => {
+        getProductoPorID();
+    }, []);  // El arreglo vacío asegura que se ejecute solo una vez al montar el componente
+
+
+
+    return (
+        <View style={styles.container}>
+            
+            <View style={styles.recuadroProducto}>
+                <Image source={{ uri:producto.imagen }} style={styles.imagen} />
+                <Text style={ styles.nombreProducto }>Producto: {producto.nombre_producto}</Text>
+                <Text>Estado: {producto.estado_del_producto}</Text>
+                <Text>Fecha de Subasta: {producto.fecha_de_subasta}</Text>
+                <Text>Hora de Subasta: {producto.hora_de_subasta}</Text>
+                <Text>Ubicación: {producto.ubicacion}</Text>
+                <Text>Precio Base: ${producto.precio_base}</Text>
+                <Text>Vendido: {producto.vendido ? "Sí" : "No"}</Text>
+            </View>
+            
+        </View>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        //justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#007BFF', 
+    },
+
+    recuadroProducto: {
+        marginTop: 20,
+        padding: 15,
+        borderWidth: 1,
+        borderRadius: 10,
+        borderColor: '#ddd',
+        backgroundColor: '#f9f9f9',
+    },
+
+    imagen: {
+        width: 180,
+        height: 180,
+        borderRadius: 10,
+    },
+
+    nombreProducto: {
+        fontSize: 18,
+        fontWeight: 'bold',
+    }
+
+  });
