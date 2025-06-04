2cf4728357955627fb76d3fd277f1327c31683d0
ricky 06 <rickyespanol8@gmail.com>
2025-06-03 20:30:24 -0400

    feat: change garantia

--- src/screens/garantia.js
+++ src/screens/garantia.js
@@ -129,7 +129,7 @@ const obtenerNombrePorId = async (usuarioId) => {
     }
 /////////////////////////////
     try {
-    const response = await fetch('https://09a6-181-115-167-36.ngrok-free.app/send-payment-email', {
+    const response = await fetch('https://4433-190-129-196-78.ngrok-free.app/send-payment-email', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
